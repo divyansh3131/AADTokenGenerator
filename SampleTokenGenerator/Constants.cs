4de45915e53db743ae6ad505ef3bd92f42841a8b
saurgupta <saurgupta@microsoft.com>
2022-11-04 11:37:05 +0530

    added prod token generator code

---
+++ SampleTokenGenerator/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleTokenGenerator
+{
+    public class Constants
+    {
+        public const string Production = "1";
+        public const string NonProd = "2";
+        public const string Exit = "3";
+    }
+}
--- SampleTokenGenerator/Program.cs
+++ SampleTokenGenerator/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Identity.Client;
+using SampleTokenGenerator;
 using System;
 using System.Threading.Tasks;
 namespace AADAuthentication
@@ -8,13 +9,45 @@ namespace AADAuthentication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder
-            .Create("18b712d3-b698-4868-adbb-540a5f105ed8")
-            .WithClientSecret("client-secret")
-            .WithAuthority(AadAuthorityAudience.AzureAdMyOrg)
-            .WithTenantId("72f988bf-86f1-41af-91ab-2d7cd011db47")
-            .Build();
-            string[] scopes = new string[] { "dde76769-8f43-4a65-bdb4-dd26f55964f8/.default" }; //New-Relic app id
+            IConfidentialClientApplication app = null;
+            string[] scopes = null;
+            string environment;
+            do
+            {
+                Console.WriteLine("Choose environment - (1)Prod/(2)Non-prod(Canary, Dev)/(3)Exit program - ");
+                environment = Console.ReadLine();
+                if (environment.Trim().Equals(Constants.Production))
+                {
+                    app = ConfidentialClientApplicationBuilder
+                    .Create("b084f5e3-4c0c-4df0-b9b7-f417f0824ba0")
+                    .WithClientSecret("Pbp8Q~fmckHhapRjLoXf3v0UMolRO1U7iSf5TcFA")
+                    .WithAuthority(AadAuthorityAudience.AzureAdMyOrg)
+                    .WithTenantId("72f988bf-86f1-41af-91ab-2d7cd011db47")
+                    .Build();
+
+                    scopes = new string[] { "cf04114d-e2e9-46c4-a332-b4ff991702e9/.default" }; //New-Relic app id
+                }
+                else if (environment.Trim().Equals(Constants.NonProd))
+                {
+                    app = ConfidentialClientApplicationBuilder
+                    .Create("18b712d3-b698-4868-adbb-540a5f105ed8")
+                    .WithClientSecret("z5t8Q~br1Cb8HfpTQv9DpW5IJjQ4M9skTezqkb6W")
+                    .WithAuthority(AadAuthorityAudience.AzureAdMyOrg)
+                    .WithTenantId("72f988bf-86f1-41af-91ab-2d7cd011db47")
+                    .Build();
+
+                    scopes = new string[] { "dde76769-8f43-4a65-bdb4-dd26f55964f8/.default" }; //New-Relic app id
+                }
+                else if(environment.Trim().Equals(Constants.Exit))
+                {
+                    Console.WriteLine("Bye...!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Please try again!");
+                }
+            } while (int.Parse(environment.Trim()) > 3);
             try
             {
                 Task<AuthenticationResult> result = app.AcquireTokenForClient(scopes)
@@ -23,7 +56,7 @@ namespace AADAuthentication
                 Console.WriteLine("Token: " + res.AccessToken);
 
             }
-            catch (MsalServiceException ex)
+            catch (MsalServiceException)
             {
             }
         }
